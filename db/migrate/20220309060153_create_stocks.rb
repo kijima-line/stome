0b6ce37726ae4bcebbcde3f031091b7b9c08f024
jun <marugo2792@gmail.com>
2022-03-09 15:38:55 +0900

    投稿機能実装

--- db/migrate/20220309060153_create_stocks.rb
+++ db/migrate/20220309060153_create_stocks.rb
@@ -1,8 +1,7 @@
 class CreateStocks < ActiveRecord::Migration[6.0]
   def change
     create_table :stocks do |t|
-      t.string :name
-
+      t.string :name, null: false
       t.timestamps
     end
   end
