3aa848f400afd8c940883797f8be3941349e3d5e
kijima-line <marugo2792@gmail.com>
2022-03-14 13:04:01 +0900

    user単体テスト日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :stocks
          has_many :mees, dependent: :destroy
+         validates :name, presence: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nameが空では登録できない' do
+      binding.pry
+     
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "名前 が入力されていません。"
+    end
+    
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "メールアドレス が入力されていません。"
+    end
+    it 'emailに@が含まれていない場合登録できない' do
+      @user.email = 'furima.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('メールアドレス は有効でありません。')
+    end
+
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワード が入力されていません。")
+    end
+    it 'password_confirmationが空では登録できない' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("パスワード確認 が内容とあっていません。")
+    end
+
+  end
+end
