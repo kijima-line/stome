c5b96a87cad86d07280b188c0c14897f68c418cf
kijima-line <marugo2792@gmail.com>
2022-03-16 10:31:25 +0900

    詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @stocks = Stock.order("created_at DESC").includes(:user)
+    @mees = Mee.order("published_at ASC").includes(:user)
     @user = User.find(params[:id])
   end
 end
