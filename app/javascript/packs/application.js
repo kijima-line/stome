da856984093d0dca191928fec4180d11ae5fed3f
kijima-line <marugo2792@gmail.com>
2022-03-21 18:16:25 +0900

    AWS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@
 //= require bootstrap
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
