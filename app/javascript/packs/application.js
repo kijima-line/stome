a5b20e1913278caff669ecbd764e21a4d788d036
kijima-line <marugo2792@gmail.com>
2022-04-10 19:01:34 +0900

    like.js_削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 require("../memo")
-require("../like")
 require("@fortawesome/fontawesome-free/js/all")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
