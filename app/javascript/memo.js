4670e7f9c0181d0447305afe30657f5dfd6aaabb
kijima-line <marugo2792@gmail.com>
2022-04-11 11:22:39 +0900

    修正

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -14,3 +14,15 @@ window.addEventListener('turbolinks:load', function() {
     });
   });
 });
+ 
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "likes/like");
+xhr.onreadystatechange  = function (e) {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      // 処理
+    } else {
+      //
+    }
+  }
+};
