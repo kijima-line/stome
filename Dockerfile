e8ef12994fcd4513e5ad9d5b05f66b232e0d6f8f
jun <97328580+kijima-line@users.noreply.github.com>
2022-06-13 09:58:11 +0900

    再設定

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM ruby:2.6.5
 
 RUN curl https://deb.nodesource.com/setup_12.x | bash
+
 RUN curl https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 RUN echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 RUN apt-get update && apt-get install -y nodejs yarn postgresql-client   
-
+RUN yarn install --check-files
+RUN yarn upgrade
 RUN mkdir /app_name 
 
 ENV APP_ROOT /app_name 
--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,11 +1,10 @@
-
-window.addEventListener('turbolinks:load', function () {
+window.addEventListener("turbolinks:load", function () {
   $(function () {
-    let topBtn = $('#side-a');
+    let topBtn = $("#side-a");
     topBtn.hide().fadeIn("");
     //スクロールが100に達したら表示
 
-    $(window).on('scroll', function () {
+    $(window).on("scroll", function () {
       if ($(this).scrollTop() > 200) {
         topBtn.fadeIn();
       } else {
@@ -22,7 +21,7 @@ let is_note_msg = true;
 //   }
 // }
 window.onscroll = function () {
-  let headerH = document.getElementsByClassName('h2')[0].clientHeight; // headerの高さを取得
+  let headerH = document.getElementsByClassName("h2")[0].clientHeight; // headerの高さを取得
   // let side_authorH = document.getElementsByClassName('side')[0].clientHeight; // 一つ目サイドバーの高さを取得
   // let side_searchH = document.getElementsByClassName('main')[0].clientHeight; // 二つ目のサイドバーの高さを取得
   // let side_categoryH = document.getElementsByClassName('list')[0].clientHeight; // 三つ目のサイドバーの高さを取得
@@ -30,7 +29,7 @@ window.onscroll = function () {
   let totalH = headerH;
   //+ side_authorH + side_searchH + side_categoryH;
   // 追従させたい要素のクラス名を取得する
-  let side_adv = document.getElementsByClassName('h2')[0];
+  let side_adv = document.getElementsByClassName("h2")[0];
 
   // 画面に追従するサイドバーの実装
   // 画面の幅が991px以下の場合は実行されないようにする
@@ -38,10 +37,10 @@ window.onscroll = function () {
     // 追従したいサイドバーのところまできたら追従する
     if (window.pageYOffset >= totalH) {
       // 追従機能に使用するCSSを設定
-      side_adv.classList.add('fixed');
+      side_adv.classList.add("fixed");
     } else {
       // 追従が必要無い箇所では追従CSSをはずず
       //side_adv.classList.remove('fixed');
     }
   }
-}
+};
